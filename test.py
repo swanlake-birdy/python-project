baeefb4a09e178ae11396e9fd364d70d47939f73
swanlake-birdy <155507982+swanlake-birdy@users.noreply.github.com>
2024-07-02 14:37:22 -0300

    updated test

--- test.py
+++ test.py
@@ -1,9 +1,87 @@
+#actuall run the slot machine
+import random
 
 #global constant
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+#HOW MANY symbol are in each reel(col)
+
+symbol_count = {
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+symbol_count = {
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        
+        else:
+            winnings += values[symbol] * bet
+
+    
+    return winnings
+
+
+
+#generating the outcome of the slot machine
+
+def get_slot_machine_spin(rows, cols, symbols):
+    #randomly pick number of symbols
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+#we need to now generate values inside the columns
+    columns = []
+    for col in range(cols):
+        column = []
+        #this make a copy of all symbol list so we dont use or update the orignal
+        current_symbol = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbol)
+            current_symbol.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+
+def print_slot_machine(columns):
+    #transposing
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], end="|")
+
+            else:
+                print(column[row], end="")
+
+        print()
+
+
 
 #collecting user input
 def deposit():
@@ -50,11 +128,10 @@ def get_bet():
         bet = input("Enter a bet amount, how much would you like to bet on each line? $")
         if bet.isdigit():
             bet = int(bet)
-            bet = int(bet)
             if MIN_BET <= bet <= MAX_BET:
                 break
             else:
-                print("Amount must be between ${MIN_BET} - ${MAX_BET}.")
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("please enter a valid number")
 
@@ -69,11 +146,26 @@ def main():
     balance = deposit()
     lines = get_number_of_lines()
     #we need to check the entered bet amount is in range of the deposit money
-    
-    bets = get_bet()
+    while True:
+        bets = get_bet()
+        total_bet = bets*lines
 
-    total_bet = bets*lines
+        if total_bet > balance:
+            print(f"You are not allowed to bet more than your deposit amount, that is $ {balance}")
+
+        else:
+            break
+        
     print(f"you are betting ${bets} on {lines} line. Total bet is {total_bet}")
+
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+
+    print_slot_machine(slots)
+
+
+
+
 
 
 #call main function
